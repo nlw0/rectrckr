Fixing quaternion class. Normalizing does not change the signs now, so the canonical form is now correct.
I'm trying to figure out what is wrong with the gradient
calculation... Going to make a few tests, and picking little problems
like this in the way.

rectrckr/corisco/__init__.py
@@ -27,10 +27,36 @@ def hess_f(x,*fargs):
 ##
 ######################################################################
 
+
+def aligned_quaternion(v):
+    ## The largest component
+    ll = np.argmax(np.abs(v))
+    ee = np.zeros(3)
+    ee[ll] = np.sign(v[ll])
+    q=Quat(np.cross(v, ee))
+    R = q.sqrt()
+
+    if np.sign(v[ll]) > 0:
+        if ll == 1:
+            R = R*Quat(1,-1,-1,-1).normalize()
+        if ll == 2:
+            R = R*Quat(1,1,1,1).normalize()
+
+    if np.sign(v[ll]) < 0:
+        if ll == 0:
+            R = R*Quat(0,0,1,0).normalize()
+        if ll == 1:
+            R = R*Quat(1,1,-1,1).normalize()
+        if ll == 2:
+            R = R*Quat(1,-1,-1,1).normalize()
+    return R.inverse()
+
+
 class Corisco():
 
     def __init__(self, edgels):
         self.edgels = edgels
+        self.Ned = self.edgels.shape[0]
 
         #focal_distance = 847.667796003
         #p_point = array([500, 375.0])
@@ -41,35 +67,84 @@ class Corisco():
         self.sqp_funcs = (val_c, grad_c, hess_c, val_f, grad_f, hess_f)
         
         ## Error function parameters (Tukey bisquare (3) with scale=0.15)
-        self.fp = array([3,1,0.15])
-        #self.fp = array([3,1,1.0])
-        #self.fp = array([2.0,1,0.1])
-        #self.fp = array([0.0,1])
-        #self.fp = array([1.0,1])
-        #self.fp = array([4.0,1,.15])
+        #self.loss = array([3.0,1,0.15])
+        #self.loss = array([3,1,1.0])
+        #self.loss = array([2.0,1,0.1])
+        self.loss = array([0.0,1])
+        #self.loss = array([1.0,1])
+        #self.loss = array([4.0,1,.15])
         ## Intrinsic parameters. pinhole mode (0)
         self.i_param = array([0.0, focal_distance, p_point[0], p_point[1]])
 
-    def estimate_orientation(self, qini):
-        args_f = (self.edgels[~isnan(self.edgels[:,2])], self.i_param, self.fp)
+        self.normals = corisco_aux.calculate_normals(self.edgels, self.i_param)
 
-        filterSQPout = filter_sqp.filterSQP(qini.q, .0, 1e-3,
+    def estimate_orientation(self, qini):
+        # args_f = (self.edgels[~isnan(self.edgels[:,2])], self.i_param, self.loss)
+        args_f = (self.edgels, self.i_param, self.loss)
+        
+        filterSQPout = filter_sqp.filterSQP(qini.q, .0, 1e-6,
                                             self.sqp_funcs,
                                             args_f)
 
         xo, err, sqp_iters,Llam,Lrho = filterSQPout
-        self.qopt = Quat(xo)
-        return self.qopt
-    
+        return Quat(xo)
 
-    def target_function(self, x, fp=None):
-        if fp is None:
-            fp = self.fp
-        args_f = (self.edgels[~isnan(self.edgels[:,2])], self.i_param, fp)
+    def random_search(self, initial_trials):
+        ## Estimate solution using RANSAC
+        bestv = np.Inf ## Smallest value found
+        args_f = (self.edgels, self.i_param, self.loss)
+        for k in range(initial_trials):
+            ## Pick indices of the reference normals. Re-sample until we
+            ## get a list of three different values.
+            pk_a = np.random.random_integers(0,self.Ned-1)
+            pk_b = np.random.random_integers(0,self.Ned-1)
+            while pk_b == pk_a:
+                pk_b = np.random.random_integers(0,self.Ned-1)
+            pk_c = np.random.random_integers(0,self.Ned-1)
+            while pk_c == pk_a or pk_c == pk_b:
+                pk_c = np.random.random_integers(0,self.Ned-1)
+
+            ## Get the normals with the first two chosen indices, and
+            ## calculate a rotation matrix that has the x axis aligned to
+            ## them.
+            n_a = self.normals[pk_a]
+            n_b = self.normals[pk_b]
+            vp1 = np.cross(n_a, n_b)
+            vp1 = vp1 * (vp1**2).sum()**-0.5
+            q1 = aligned_quaternion(vp1)
+
+            ## Pick a new random third norm, and find the rotation to align
+            ## the y direction to this edgel.
+            n_c = self.normals[pk_c]
+            vaux = np.dot(n_c, q1.rot())
+            ang = np.arctan2(vaux[1], -vaux[2])
+            q2 = Quat(np.sin(ang/2),0,0) * q1 ## The resulting orientation
+
+            ## Find the value of the target function for this sampled
+            ## orientation.
+            newv = corisco_aux.angle_error(q2.q, *args_f) 
+
+            ## If the value is the best yet, store solution.
+            if newv <= bestv :
+                bestv = newv
+                bpk_a = pk_a
+                bpk_b = pk_b
+                bpk_c = pk_c
+                qopt = q2
+        return qopt, bpk_a, bpk_b, bpk_c
+
+    def target_function_value(self, x, loss=None):
+        if loss is None:
+            loss = self.loss
+        # args_f = (self.edgels[~isnan(self.edgels[:,2])], self.i_param, loss)
+        args_f = (self.edgels, self.i_param, loss)
         return corisco_aux.angle_error(x, *args_f) 
 
-    def gradient_function(self, x):
-        args_f = (self.edgels[~isnan(self.edgels[:,2])], self.i_param, self.fp)
+    def target_function_gradient(self, x, loss=None):
+        if loss is None:
+            loss = self.loss
+        # args_f = (self.edgels[~isnan(self.edgels[:,2])], self.i_param, loss)
+        args_f = (self.edgels, self.i_param, loss)
         return corisco_aux.angle_error_gradient(x, *args_f) 
 
     def plot_edgels(self, ax):
@@ -78,7 +153,7 @@ class Corisco():
                 (self.edgels[:,[1,1]] + scale*np.c_[-self.edgels[:,2], self.edgels[:,2]]).T,
                 '-',lw=3.0,alpha=1.0,color='#ff0000')
 
-    def plot_vps(self, ax):
+    def plot_vps(self, ax, qopt):
         #############################################################
         ## Plot the vanishing point directions at various pixels. ax
         ## is a matplotlib axes, taken with "gca()". Spacing the
@@ -91,7 +166,7 @@ class Corisco():
         by = 0.+(Iheight/2)%spacing
         qL = np.mgrid[bx:Iwidth:spacing,by:Iheight:spacing].T.reshape((-1,2))
         Nq = qL.shape[0]
-        vL = corisco_aux.calculate_vdirs(self.qopt.q, np.array(qL, dtype=np.float32), self.i_param)
+        vL = corisco_aux.calculate_vdirs(qopt.q, np.array(qL, dtype=np.float32), self.i_param)
         LL = np.zeros((3,Nq,4))
         for lab in range(3):
             for num in range(Nq):

rectrckr/corisco/quaternion.py
@@ -58,8 +58,6 @@ class Quat:
         return np.sqrt((self.q**2).sum())
     def normalize(self):
         self.q = self.q / self.norm()
-        if self.q[0]<0:
-            self.q = self.q * -1
         return self
 
     def __mul__(self,bb):
@@ -114,18 +112,17 @@ class Quat:
 
         r = np.array([0.,0,0,0])
 
-        if v1 >= v2 and v1>=v3:
+        if v1 >= v2 and v1 >= v3:
             r[ii[0]] = np.sign(self.q[ii[0]])
-        elif v2 > v1 and v2>=v3:
+        elif v2 > v1 and v2 >= v3:
             r[ii[0]] = np.sign(self.q[ii[0]])
             r[ii[1]] = np.sign(self.q[ii[1]])
         else:
-            r = np.sign(self.q)
+            r = np.sign(self.q) 
+        print r
         r=Quat(r)
         r.normalize()
         return r.inverse() * self
-
-
 
 
 def matrix_to_quaternion(Q):
@@ -164,7 +161,6 @@ def matrix_to_quaternion(Q):
     out[p] = np.array([qu,qv,qw])*nf
     
     return out
-
 
 
 if __name__ == '__main__':

rectrckr/tools/rtr_simulate_edgels.py
@@ -7,6 +7,7 @@ import rectrckr
 import rectrckr.lowlevel as lowlevel
 
 from pylab import *
+import scipy.misc
 
 from rectrckr.kalman_filter import KalmanFilter
 
@@ -70,7 +71,6 @@ def main():
     ori_correct = Quat(cos(answer), sin(answer), 0.0, 0.0)
     edgels = cc.project(ori_correct)
 
-
     co = corisco.Corisco(edgels)
     
 
@@ -93,12 +93,12 @@ def main():
     styles=[]
     for fp in fps:
         ff = array([
-                co.target_function(Quat(sqrt(1.0 - tt ** 2), tt, 0.0, 0.0).q, fp)
+                co.target_function_value(Quat(sqrt(1.0 - tt ** 2), tt, 0.0, 0.0).q, fp)
                 for tt in sin(angles)
                 ])
 
         styles.append(plot(-angles*2*180/pi, ff, lw=2, color=ccc[ck])[0])
-        ck+=1
+        ck += 1
 
     plot([-answer*2*180/pi, -answer*2*180/pi], [0.0,2.0], 'k--')
     grid()
@@ -106,25 +106,62 @@ def main():
     legend(styles, ('Absolute','Quadratic','Tukey bisquare'), loc='lower right')
 
 
-    figure()
-    title('rectrckr simulation')
     tt = -pi/12
+    #tt = -pi/12 + 0.2
     qini = Quat(sqrt(1.0 - tt ** 2), tt, 0.0, 0.0)
 
     co = corisco.Corisco(edgels)
-    qopt = co.estimate_orientation(qini=qini)
+    co.qopt = ori_correct
 
-    print qini
-    print qopt
-    print ori_correct, co.target_function(ori_correct.q),\
-        co.gradient_function(ori_correct.q)
-
-    ion()
+    figure()
     ax = subplot(1,1,1)
+    title('rectrckr simulation')
+
     axis('equal')
     co.plot_vps(ax)
     co.plot_edgels(ax)
     axis([0,640,480,0])
 
+    co.qopt = qini
+
+    figure()
+    ax = subplot(1,1,1)
+    title('Estimation initial state')
+    axis('equal')
+    co.plot_vps(ax)
+    co.plot_edgels(ax)
+    axis([0,640,480,0])
+
+
+    qopt = co.estimate_orientation(qini=qini)
+
+    print qini, co.target_function_value(qini.q)
+    print qopt, co.target_function_value(qopt.q)
+    print ori_correct, co.target_function_value(ori_correct.q)
+
+    dd = 1e-6
+    print 'calgrad', co.target_function_gradient(ori_correct.q)
+    numgrad = array([
+        (co.target_function_value(ori_correct.q + array([dd,0,0,0])) -
+         co.target_function_value(ori_correct.q - array([dd,0,0,0]))) / (2*dd),
+        (co.target_function_value(ori_correct.q + array([0,dd,0,0])) -
+         co.target_function_value(ori_correct.q - array([0,dd,0,0]))) / (2*dd),
+        (co.target_function_value(ori_correct.q + array([0,0,dd,0])) -
+         co.target_function_value(ori_correct.q - array([0,0,dd,0]))) / (2*dd),
+        (co.target_function_value(ori_correct.q + array([0,0,0,dd])) -
+         co.target_function_value(ori_correct.q - array([0,0,0,dd]))) / (2*dd)
+        ])
+    print 'numgrad', numgrad
+
+
+    figure()
+    ax = subplot(1,1,1)
+    title('Estimation result')
+    axis('equal')
+    co.plot_vps(ax)
+    co.plot_edgels(ax)
+    axis([0,640,480,0])
+
+    
     code.interact(local=locals())
 